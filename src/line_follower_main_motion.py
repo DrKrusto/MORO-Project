320429e4be791d96eba54e03d3c185f3a54e7a2d
BABAdu95 <baptiste.lorenzi@edu.esiee-it.fr>
2024-01-23 21:23:55 +0100

    Updated config file and Rviz

--- src/line_follower_main_motion.py
+++ src/line_follower_main_motion.py
@@ -3,69 +3,63 @@
 # Author: Markus Luftensteiner
 
 import rospy
-from geometry_msgs.msg import Twist
-from nav_msgs.msg import Odometry
-from std_msgs.msg import String, Float32, Int32
+from geometry_msgs import Twist
+from nav_msgs import Odometry
+from std_msgs import String, Float32
 from math import atan2, sqrt, hypot
 
 class SimplePoseController:
     def __init__(self) -> None:
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-        self.sub = rospy.Subscriber('/line_position', Int32, self.line_position_callback)
-        self.sub2 = rospy.Subscriber('/line_x_position', Float32, self.line_x_position_callback)
+        self.sub = rospy.Subscriber('/motion', String, self.motion_callback)
+        self.sub2 = rospy.Subscriber('/steering', Float32, self.steering_callback)
         self.rate = rospy.Rate(1)
 
-    # Controls the motion of the robot
-    def line_position_callback(self, msg:Int32):
-        if rospy.get_param('steering_method') == 'bangbang':
-            position = msg.data
-            speed : Twist = Twist()
+    # Controls the motion of the robot -> forward, backward, clockwise, counterclockwise, stop
+    def motion_callback(self, msg:String):
+        command = msg.data
+        speed : Twist = Twist()
 
-            if position == 0: # center
-                speed.linear.x = 0.22
-            elif position > 0: # right
-                speed.angular.z = 0.3
-            elif position < 0: # left
-                speed.angular.z = -0.3
-            else: # ignore all other values
-                pass
-            
-            self.pub.publish(speed)
+        if command == "forward":
+            speed.linear.x = 0.22
+        elif command == "backward":
+            speed.linear.x = -0.22
+        elif command == "clockwise":
+            speed.angular.z = 0.3
+        elif command == "counterclockwise":
+            speed.angular.z = -0.3
+        elif command == "stop":
+            speed.linear.x = 0
+            speed.angular.z = 0
+        else: # ignore all other values
+            pass
+        
+        self.pub.publish(speed)
     
-    def line_x_position_callback(self, action:Float32) -> None:
-        if rospy.get_param('steering_method') == 'proportional':
-            action_val = float(action.data)
-            speed : Twist = Twist()
+    def steering_callback(self, action:Float32) -> None:
+        action_val = float(action.data)
+        speed : Twist = Twist()
 
-            # Message is centered on the image, negative = left, positive = right
-            if action_val < 0: # Rotate left
-                speed.linear.x = 0.0
-                speed.angular.z = -0.3
-            elif action_val > 0: # Rotate right
-                speed.linear.x = 0.0
-                speed.angular.z = 0.3
-            elif action_val == 0: # Move forward
-                speed.linear.x = 0.22
-                speed.angular.z = 0
-            else: # In all other cases stop the robot
-                speed.linear.x = 0.0
-                speed.angular.z = 0.0
+        # Message is centered on the image, negative = left, positive = right
+        if action_val < 0: # Rotate left
+            speed.linear.x = 0.0
+            speed.angular.z = -0.3
+        elif action_val > 0: # Rotate right
+            speed.linear.x = 0.0
+            speed.angular.z = 0.3
+        elif action_val == 0: # Move forward
+            speed.linear.x = 0.22
+            speed.angular.z = 0
+        else: # In all other cases stop the robot
+            speed.linear.x = 0.0
+            speed.angular.z = 0.0
 
-            self.pub.publish(speed)
-            rospy.sleep(0.01)
-
-    def switch_steering_method(self) -> None:
-        if rospy.get_param('steering_method') == 'proportional':
-            rospy.set_param('steering_method', 'bangbang')
-        else:
-            rospy.set_param('steering_method', 'proportional')
+        self.pub.publish(speed)
+        rospy.sleep(0.01)
 
 
 if __name__ == '__main__':
     rospy.init_node('motion_node')
-
-    rospy.set_param('steering_method', 'bangbang') # Default mode on 
-
     simple_pose_controller = SimplePoseController()
 
     rospy.spin()
